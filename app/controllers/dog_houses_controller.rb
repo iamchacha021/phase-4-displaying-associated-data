95196ef4762768ba755968adbedec28c936be26a
iamchacha021 <iamchacha021@gmail.com>
2023-03-26 17:17:02 +0300

    done

--- app/controllers/dog_houses_controller.rb
+++ app/controllers/dog_houses_controller.rb
@@ -1,9 +1,13 @@
 class DogHousesController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
+  def index
+    render json: DogHouse.all
+  end
+
   def show
     dog_house = DogHouse.find(params[:id])
-    render json: dog_house
+    render json: dog_house, include: :reviews 
   end
 
   private
